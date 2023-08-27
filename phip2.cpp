821eeeef3b24f0c72993addcc0aed647f71f13c5
oliviergimenez <olivier.gimenez@cefe.cnrs.fr>
2023-08-27 16:26:53 +0300

    Good to go!

---
+++ phip2.cpp
@@ -0,0 +1,128 @@
+// linear regression
+#include <TMB.hpp>
+
+//template<class Type>
+//matrix<Type> multmat(array<Type> A, matrix<Type> B) {
+//  int nrowa = A.rows();
+//  int ncola = A.cols(); 
+//  int ncolb = B.cols(); 
+//  matrix<Type> C(nrowa,ncolb);
+//  for (int i = 0; i < nrowa; i++)
+//  {
+//    for (int j = 0; j < ncolb; j++)
+//    {
+//      C(i,j) = Type(0);
+//      for (int k = 0; k < ncola; k++)
+//        C(i,j) += A(i,k)*B(k,j);
+//    }
+//  }
+//  return C;
+//}
+//
+/* implement the vector - matrix product */
+template<class Type>
+vector<Type> multvecmat(array<Type>  A, matrix<Type>  B) {
+  int nrowb = B.rows();
+  int ncolb = B.cols(); 
+  vector<Type> C(ncolb);
+  for (int i = 0; i < ncolb; i++)
+  {
+    C(i) = Type(0);
+    for (int k = 0; k < nrowb; k++){
+      C(i) += A(k)*B(k,i);
+    }
+  }
+  return C;
+}
+
+template<class Type>
+Type objective_function<Type>::operator() () {
+  
+  // b = parameters
+  PARAMETER(phib);
+  PARAMETER(detb);
+  
+  // ch = capture-recapture histories (individual format)
+  // fc = date of first capture
+  // fs = state at first capture
+  DATA_IMATRIX(ch);
+  DATA_IVECTOR(fc);
+  DATA_IVECTOR(fs);
+  
+  // OBSERVATIONS
+  // 0 = non-detected
+  // 1 = detected
+  //   
+  // STATES
+  // 1 = alive 
+  // 2 = dead
+  //   
+  // PARAMETERS
+  // phi  survival prob.
+  // p  detection prob.
+  // pi prob. of being in initial state alive
+  //   
+  // logit link for all parameters
+
+  int km = ch.rows();
+  int nh = ch.cols();  
+  vector<Type> par(2);
+  par(0) = Type(1.0) / (Type(1.0) + exp(-phib));
+  par(1) = Type(1.0) / (Type(1.0) + exp(-detb));
+  Type phi = par(0);
+  Type p = par(1);
+  ADREPORT(p);
+  ADREPORT(phi);
+  REPORT(p);
+  REPORT(phi);
+  
+//  Type pi = par(0); // careful, indexing starts at 0 in Rcpp!
+//  Type phi = par(0);
+ // Type det = par(1);
+
+  // prob of obs (rows) cond on states (col)
+  matrix<Type> B(2,2);
+  B(0,0) = Type(1.0)-par(1);
+  B(0,1) = Type(1.0);
+  B(1,0) = par(1);
+  B(1,1) = Type(0.0);
+
+  // first encounter
+  matrix<Type> BE(2, 2);
+  BE(0,0) = Type(0.0);
+  BE(0,1) = Type(1.0);
+  BE(1,0) = Type(1.0);
+  BE(1,1) = Type(0.0);
+
+  // prob of states at t+1 given states at t
+  matrix<Type> A(2, 2);
+  A(0,0) = par(0);
+  A(0,1) = Type(1.0)-par(0);
+  A(1,0) = Type(0.0);
+  A(1,1) = Type(1.0);
+  
+  // init states
+  vector<Type> PROP(2);
+  PROP(0) = Type(1.0);
+  PROP(1) = Type(0.0);
+//  PROP(0) = pi;
+//  PROP(1) = Type(1.0)-pi;
+//  REPORT(PROP);
+  
+  // likelihood
+  Type ll;
+  Type nll;
+  array<Type> ALPHA(2);
+  for (int i = 0; i < nh; i++) {
+    int ei = fc(i)-1;
+    vector<int> evennt = ch.col(i);
+    ALPHA = PROP * vector<Type>(BE.row(fs(i))); // element-wise vector product
+    for (int j = ei+1; j < km; j++) {
+      ALPHA = multvecmat(ALPHA,A) * vector<Type>(B.row(evennt(j))); // vector matrix product, then element-wise vector product
+    }
+    ll += log(sum(ALPHA));
+  }
+  nll = -ll;
+  return nll;
+}
+
---
+++ phipREt.cpp
@@ -0,0 +1,153 @@
+// linear regression
+#include <TMB.hpp>
+
+//template<class Type>
+//matrix<Type> multmat(array<Type> A, matrix<Type> B) {
+//  int nrowa = A.rows();
+//  int ncola = A.cols(); 
+//  int ncolb = B.cols(); 
+//  matrix<Type> C(nrowa,ncolb);
+//  for (int i = 0; i < nrowa; i++)
+//  {
+//    for (int j = 0; j < ncolb; j++)
+//    {
+//      C(i,j) = Type(0);
+//      for (int k = 0; k < ncola; k++)
+//        C(i,j) += A(i,k)*B(k,j);
+//    }
+//  }
+//  return C;
+//}
+//
+/* implement the vector - matrix product */
+template<class Type>
+vector<Type> multvecmat(array<Type>  A, matrix<Type>  B) {
+  int nrowb = B.rows();
+  int ncolb = B.cols(); 
+  vector<Type> C(ncolb);
+  for (int i = 0; i < ncolb; i++)
+  {
+    C(i) = Type(0);
+    for (int k = 0; k < nrowb; k++){
+      C(i) += A(k)*B(k,i);
+    }
+  }
+  return C;
+}
+
+template<class Type>
+Type objective_function<Type>::operator() () {
+  
+  // ch = capture-recapture histories (individual format)
+  // fc = date of first capture
+  // fs = state at first capture
+  DATA_IMATRIX(ch);
+  DATA_IVECTOR(fc);
+  DATA_IVECTOR(fs);
+  
+  // OBSERVATIONS
+  // 0 = non-detected
+  // 1 = detected
+  //   
+  // STATES
+  // 1 = alive 
+  // 2 = dead
+  //   
+  // PARAMETERS
+  // phi  survival prob.
+  // p  detection prob. timedep
+  // pi prob. of being in initial state alive
+  //   
+  // logit link for all parameters
+  
+  int km = ch.rows();
+  int nh = ch.cols();  
+
+  // b = parameters
+  PARAMETER(phib); Type phi = Type(1.0) / (Type(1.0) + exp(-phib)); 
+  PARAMETER(mu); // mean of the random effect
+  PARAMETER(logsig); Type sig = exp(logsig);  // random effect SD
+  PARAMETER_VECTOR(u); // random effects
+  
+//  PARAMETER_VECTOR(detb); 
+  vector<Type> p(19);
+  p(0) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(0))));
+  p(1) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(1))));
+  p(2) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(2))));
+  p(3) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(3))));
+  p(4) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(4))));
+  p(5) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(5))));
+  p(6) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(6))));
+  p(7) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(7))));
+  p(8) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(8))));
+  p(9) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(9))));
+  p(10) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(10))));
+  p(11) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(11))));
+  p(12) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(12))));
+  p(13) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(13))));
+  p(14) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(14))));
+  p(15) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(15))));
+  p(16) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(16))));
+  p(17) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(17))));
+  p(18) = Type(1.0) / (Type(1.0) + exp(-(mu + sig * u(18))));
+  
+  // first encounter
+  matrix<Type> BE(2, 2);
+  BE(0,0) = Type(0.0);
+  BE(0,1) = Type(1.0);
+  BE(1,0) = Type(1.0);
+  BE(1,1) = Type(0.0);
+  
+  // prob of states at t+1 given states at t
+  matrix<Type> A(2, 2);
+  A(0,0) = phi;
+  A(0,1) = Type(1.0)-phi;
+  A(1,0) = Type(0.0);
+  A(1,1) = Type(1.0);
+  
+  // init states
+  vector<Type> PROP(2);
+  PROP(0) = Type(1.0);
+  PROP(1) = Type(0.0);
+  //  PROP(0) = pi;
+  //  PROP(1) = Type(1.0)-pi;
+  //  REPORT(PROP);
+  
+  // likelihood
+  Type ll = 0;
+  Type nll = 0;
+  
+  // random effect
+  Type zero = 0.0;         // a constant
+  Type un = 1.0;         // a constant
+  Type kmun = km - un;
+  
+  for(int j = 0; j < kmun; j++){
+    ll += dnorm(u(j), zero, un, true);
+  }
+  array<Type> ALPHA(2);
+  for (int i = 0; i < nh; i++) {
+    int ei = fc(i)-1;
+    vector<int> evennt = ch.col(i);
+    ALPHA = PROP * vector<Type>(BE.row(fs(i))); // element-wise vector product
+    for (int j = ei+1; j < km; j++) {
+      
+      // prob of obs (rows) cond on states (col)
+      matrix<Type> B(2,2);
+      B(0,0) = Type(1.0) - p(j-1);
+      B(0,1) = Type(1.0);
+      B(1,0) = p(j-1);
+      B(1,1) = Type(0.0);
+      
+      ALPHA = multvecmat(ALPHA,A) * vector<Type>(B.row(evennt(j))); // vector matrix product, then element-wise vector product
+    }
+    ll += log(sum(ALPHA));
+  }
+  nll = -ll;
+  ADREPORT(phi);
+  ADREPORT(sig);
+  ADREPORT(mu);
+  ADREPORT(p);
+  return nll;
+}
+
